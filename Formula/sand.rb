fa891f906ac0caa9ffd854f795eaac7aba2b8af1
khoi <git@khoi.io>
2026-01-13 07:35:08 +0700

    Make Homebrew inreplace conditional

--- Formula/sand.rb
+++ Formula/sand.rb
@@ -7,12 +7,14 @@ class Sand < Formula
 
   def install
     # Avoid requiring SSH credentials during SwiftPM dependency fetches.
-    inreplace "Package.swift",
-              "git@github.com:apple/swift-log.git",
-              "https://github.com/apple/swift-log.git"
-    inreplace "Package.resolved",
-              "git@github.com:apple/swift-log.git",
-              "https://github.com/apple/swift-log.git"
+    ssh_url = "git@github.com:apple/swift-log.git"
+    https_url = "https://github.com/apple/swift-log.git"
+    if File.exist?("Package.swift") && File.read("Package.swift").include?(ssh_url)
+      inreplace "Package.swift", ssh_url, https_url
+    end
+    if File.exist?("Package.resolved") && File.read("Package.resolved").include?(ssh_url)
+      inreplace "Package.resolved", ssh_url, https_url
+    end
 
     system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/sand"
